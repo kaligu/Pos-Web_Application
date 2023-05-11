66b8026189c221698eb21638f2443e06cf3f6897
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-05-11 18:31:58 +0530

    implemented customer place order form SPA

--- js/MainScript.js
+++ js/MainScript.js
@@ -1,14 +1,20 @@
-
-
-
 $('#btnCustomerForm').click(function() {
     // Load the card.html file into the #card-container div
+    document.querySelector("#placeOrderForm").style.display="none";
     document.querySelector("#customerForm").style.display="block";
     document.querySelector("#itemForm").style.display="none";
 });
 
 $('#btnItemForm').click(function() {
     // Load the card.html file into the #card-container div
+    document.querySelector("#placeOrderForm").style.display="none";
     document.querySelector("#itemForm").style.display="block";
+    document.querySelector("#customerForm").style.display="none";
+});
+
+$('#btnPlaceorderForm').click(function() {
+    // Load the card.html file into the #card-container div
+    document.querySelector("#placeOrderForm").style.display="block";
+    document.querySelector("#itemForm").style.display="none";
     document.querySelector("#customerForm").style.display="none";
 });
